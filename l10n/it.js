4c176ae744d75156440cc18f833882a6f7dbcb25
Marco Palumbo <36733571+marcopalumbo@users.noreply.github.com>
2022-07-15 16:15:53 +0200

    added italian translation files

---
+++ l10n/it.js
@@ -0,0 +1,26 @@
+OC.L10N.register(
+    "sociallogin",
+    {
+        "Disable auto create new users": "Disabilita la creazione automatica dei nuovi utenti",
+        "Create users with disabled account": "Crea gli utenti con l'account disabilitato",
+        "Allow users to connect social logins with their account": "Permetti agli utenti di collegare il Social Login al loro account",
+        "Prevent creating an account if the email address exists in another account": "Impedisci la creazione dell'account se l'indirizzo email è già utilizzato in un'altro account",
+        "Update user profile every login": "Aggiorna il profilo utente ad ogni login",
+        "Do not prune not available user groups on login": "Non eliminare i gruppi non disponibili al login",
+        "Automatically create groups if they do not exists": "Crea automaticamente i gruppi se non esistono",
+        "Restrict login for users without mapped groups": "Limita l'accesso agli utenti non associati a nessun gruppo",
+        "Restrict login for users without assigned groups": "Limita l'accesso agli utenti non assegnati a nessun gruppo",
+        "Disable notify admins about new users": "Disabilita la notifica di creazione nuovo utente per gli utenti admin",
+
+        "None": "Nessuno",
+        "Save": "Salva",
+        "Allow login only from specified domain": "Permetti l'accesso solo da domini specifici",
+        "Allow login only for specified organizations": "Permetti l'accesso solo da organizzazioni specifiche",
+        "Allow login only for specified guilds": "Permetti l'accesso solo per da livelli specifici",
+
+        "Settings for social login successfully saved": "Configurazione del Social Login salvata con successo",
+        "Do you really want to remove {providerTitle} provider ?": "Vuoi veramente rimuovere il provider {providerTitle}?",
+        "Some error occurred while saving settings": "Si è verificato un errore dureante il salvataggio",
+        "Confirm remove": "Confermi la rimozione?"
+    },
+    "nplurals=2; plural=(n > 1);");
